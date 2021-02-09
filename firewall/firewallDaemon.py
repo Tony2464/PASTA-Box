293403e10d293ba4a877e0b96e35b83e9d9818fe
XenorInspire <nicolas.fouchard94370@gmail.com>
2021-02-09 20:02:27 +0000

    Clean code

--- firewall/firewallDaemon.py
+++ firewall/firewallDaemon.py
@@ -1,57 +1,9 @@
-import os
+import getIpAdresses
 import json
 from datetime import datetime
 from os import listdir, walk
 from os.path import isfile, join
 from crontabs import Cron, Tab
-
-# Apply all the rules
-
-def applyEbtable(IPlist, mode):
-
-    if(mode == True):
-        res = os.system("sudo ebtables -F")
-
-    for i in range(len(IPlist)):
-        cmd = "sudo ebtables -t filter -A FORWARD -p IPv4 --ip-dst " + \
-            IPlist[i].replace('\n', '') + " -j DROP"
-        res = os.system(cmd)
-        cmd = "sudo ebtables -t filter -A FORWARD -p IPv4 --ip-src " + \
-            IPlist[i].replace('\n', '') + " -j DROP"
-        res = os.system(cmd)
-    res = os.system("sudo ebtables-nft-save > /PASTA-Box/firewall/rulesBackup.txt")
-
-
-# Get all files in the repo blocklist-ipsets
-
-def fileList(source):
-    files = []
-    for root, dirnames, filenames in os.walk(source):
-        for filename in filenames:
-            files.append(os.path.join(root, filename))
-    return files
-
-
-# Fetch all ip adresses
-
-def fetchIP():
-    # print('args={} kwargs={} running at {}'.format(args, kwargs, datetime.now()))
-    res = os.system("cd blocklist-ipsets && git fetch")
-    listFiles = fileList("blocklist-ipsets/")
-    dataIP = []
-
-    for i in range(len(listFiles)):
-        with open(listFiles[i], encoding="utf8", errors="ignore") as IPfile:
-            data = IPfile.readlines()
-            IPfile.close()
-
-        for j in range(len(data)):
-            if data[j].find("#") == -1:
-                dataIP.append(data[j])
-
-    dataIP = list(dict.fromkeys(dataIP))
-    applyEbtable(dataIP, True)
-
 
 ###### Beginning of the program ######
 
@@ -61,23 +13,23 @@ with open(config, encoding="utf8", errors="ignore") as configFile:
     configFile.close()
 
 jsonData = json.loads(configData)
-if jsonData["Mode"] == 1: # Monthly job
+if jsonData["Mode"] == "1": # Monthly job
     Cron().schedule(
         Tab(name="fetchIP")
         .every(months=int(jsonData["Duration"]))
-        .run(fetchIP)
+        .run(getIpAdresses.fetchIP)
     ).go()
-elif jsonData["Mode"] == 2: # Weekly job
+elif jsonData["Mode"] == "2": # Weekly job
     Cron().schedule(
         Tab(name="fetchIP")
         .every(weeks=int(jsonData["Duration"]))
-        .run(fetchIP)
+        .run(getIpAdresses.fetchIP)
     ).go()
 else:
     Cron().schedule(
         Tab(name="fetchIP")  # Daily job
         .every(days=int(jsonData["Duration"]))
-        .run(fetchIP)
+        .run(getIpAdresses.fetchIP)
     ).go()
 
 #fetchIP()
---
+++ firewall/getIpAdresses.py
@@ -0,0 +1,48 @@
+import os
+
+# Apply all the rules
+
+def applyEbtable(IPlist, mode):
+
+    if(mode == True):
+        res = os.system("sudo ebtables -F")
+
+    for i in range(len(IPlist)):
+        cmd = "sudo ebtables -t filter -A FORWARD -p IPv4 --ip-dst " + \
+            IPlist[i].replace('\n', '') + " -j DROP"
+        res = os.system(cmd)
+        cmd = "sudo ebtables -t filter -A FORWARD -p IPv4 --ip-src " + \
+            IPlist[i].replace('\n', '') + " -j DROP"
+        res = os.system(cmd)
+    res = os.system(
+        "sudo ebtables-nft-save > /PASTA-Box/firewall/rulesBackup.txt")
+
+# Get all files in the repo blocklist-ipsets
+
+
+def fileList(source):
+    files = []
+    for root, dirnames, filenames in os.walk(source):
+        for filename in filenames:
+            files.append(os.path.join(root, filename))
+    return files
+
+
+# Fetch all ip adresses
+
+def fetchIP():
+    res = os.system("cd blocklist-ipsets && git fetch")
+    listFiles = fileList("blocklist-ipsets/")
+    dataIP = []
+
+    for i in range(len(listFiles)):
+        with open(listFiles[i], encoding="utf8", errors="ignore") as IPfile:
+            data = IPfile.readlines()
+            IPfile.close()
+
+        for j in range(len(data)):
+            if data[j].find("#") == -1:
+                dataIP.append(data[j])
+
+    dataIP = list(dict.fromkeys(dataIP))
+    applyEbtable(dataIP, True)

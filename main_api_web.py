af4fb586929c772146b9517e8315f85bf98ee93d
XenorInspire <nicolas.fouchard94370@gmail.com>
2021-02-15 21:11:28 +0000

    ADD api firewall rules

--- main_api_web.py
+++ main_api_web.py
@@ -6,9 +6,11 @@ import web.conf.config as config
 
 # Import all routes
 from api.routes.frames import frames
+from api.routes.rules import rules
 
 app = Flask(__name__)
 app.register_blueprint(frames, url_prefix="/api/frames")
+app.register_blueprint(rules, url_prefix="/api/rules")
 
 @app.route('/')
 def home():

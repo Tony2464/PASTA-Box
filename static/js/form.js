1fda68a3d4461594b12511de0d88bd67e76e7e0e
XenorInspire <nicolas.fouchard94370@gmail.com>
2021-03-08 18:41:27 +0000

    ADD disabling form inputs js script

---
+++ static/js/form.js
@@ -0,0 +1,14 @@
+function enableInput(id) {
+
+    let input = document.getElementById(id);
+    input.disabled = false;
+
+}
+
+function disableInput(id) {
+
+
+    let input = document.getElementById(id);
+    input.disabled = true;
+
+}

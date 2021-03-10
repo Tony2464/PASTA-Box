47697dee83b634f2cba15e27c699ca627e07e170
XenorInspire <nicolas.fouchard94370@gmail.com>
2021-03-10 08:07:16 +0000

    ADD js scripts for the firewall rule form

--- static/js/form.js
+++ static/js/form.js
@@ -12,3 +12,27 @@ function disableInput(id) {
     input.disabled = true;
 
 }
+
+function displayError(error) {
+
+    flushAlerts();
+    let divAlert = document.createElement('div');
+    let danger = document.createElement('strong');
+    const target = document.querySelector('#errorAnchor');
+
+    danger.innerHTML = "Error ! "
+    divAlert.className = "alert alert-danger";
+    target.parentNode.insertBefore(divAlert, target.nextSibling);
+    divAlert.appendChild(danger);
+    divAlert.innerHTML += error;
+
+}
+
+function flushAlerts() {
+
+    let alerts = document.getElementsByClassName('alert');
+    if (alerts != null)
+        for (let i = 0; i < alerts.length; i++)
+            alerts[i].remove();
+
+}
---
+++ static/js/network.js
@@ -0,0 +1,44 @@
+var regexIPv4 = new RegExp('^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$');
+var regexIPv6 = new RegExp('(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))');
+
+function checkPort(id) {
+
+    let input = document.getElementById(id).value;
+    if (input < 1 || input > 65535)
+        displayError("Invalid port");
+    else
+        flushAlerts();
+
+}
+
+function checkIP(id) {
+
+    let input = document.getElementById(id).value;
+    if (checkIpVersion(input) != 0)
+        testSameIpVersion();
+    else
+        displayError("Invalid IP address");
+
+}
+
+function testSameIpVersion() {
+
+    let ip1 = document.getElementById('ipAddrSrc').value;
+    let ip2 = document.getElementById('ipAddrDst').value;
+
+    if (ip1 != "" && ip2 != "")
+        if (checkIpVersion(ip1) != checkIpVersion(ip2))
+            displayError("The firewall rule can handle one type of IP version at the time");
+
+}
+
+function checkIpVersion(IPaddress) {
+
+    if (regexIPv4.test(IPaddress) == true)
+        return 1;
+    else if (regexIPv6.test(IPaddress) == true)
+        return 2;
+    else
+        return 0;
+
+}

17ca16b20e11071037f51d53f6f147452d3215d4
Tony2464 <anthony.fargette@yahoo.fr>
2021-03-18 15:59:26 +0100

    UPDATE capture with one file HOTFIX capture stopped

--- network_capture/main_capture.py
+++ network_capture/main_capture.py
@@ -2,9 +2,28 @@
 
 import os
 import sys
+from subprocess import Popen, PIPE
 
 # local imports
 from conf import config as config
+from database import db_manager
+
+dbManager = db_manager.DbManager(
+    config.dbConfig["user"],
+    config.dbConfig["password"],
+    config.dbConfig["host"],
+    config.dbConfig["port"],
+    config.dbConfig["database"]
+)
+
+# Insert information
+count = 0
+srcport = ""
+dstport = ""
+protocol = ""
+applicationLayer = ""
+srcIp = ""
+dstIp = ""
 
 insertProgram = "insert_capture.py"  # With extension
 separator = "|"
@@ -52,12 +71,59 @@ def main():
             formattedFields += "-e " + fields[i] + " "
 
         # Executing the capture
-        os.system("sudo tshark " +
-                  buffer +
-                  " -i " + config.interface +
-                  " -T fields -E separator='" + separator + "' " +
-                  formattedFields +
-                  " | " + "python3 " + insertProgram)
+        # os.system("sudo tshark " +
+        #           buffer +
+        #           " -i " + config.interface +
+        #           " -T fields -E separator='" + separator + "' " +
+        #           formattedFields +
+        #           " | " + "python3 " + insertProgram)
+        
+        cmd = "sudo tshark " + buffer + " -i " + config.interface + " -T fields -E separator='" + separator + "' " + formattedFields
+        with Popen(cmd, shell=True, stdout=PIPE, bufsize=1, universal_newlines=True) as p:
+            for line in p.stdout:
+                frame = line.split("|")
+
+                # Determine tcp port or udp port
+                if frame[0]:  # Tcp port
+                    srcport = frame[0]
+                    dstport = frame[1]
+                else:  # Udp port
+                    srcport = frame[2]
+                    dstport = frame[3]
+
+                # Determine ipv4 or ipv6
+                if frame[4]:  # ipv4
+                    srcIp = frame[4]
+                    dstIp = frame[5]
+                else:
+                    srcIp = frame[6]
+                    dstIp = frame[7]
+
+                # Determine application layer
+                protocol = frame[10].split(":")
+                if len(protocol) > 2:
+                    applicationLayer = protocol[4] if len(protocol) == 5 else None
+                    networkLayer = protocol[2]
+                else:
+                    applicationLayer = None
+                    networkLayer = None
+                # Insertion
+                dbManager.queryInsert(
+                    "INSERT INTO `Frame` (`portSource`, `portDest`, `ipSource`, `ipDest`, `macAddrSource`, `macAddrDest`, `protocolLayerApplication`, `protocolLayerTransport`, `protocolLayerNetwork`, `date`, `domain`, `info`) VALUES(%s, %s, %s, %s,%s, %s, %s, %s, %s, CURRENT_TIME(), %s, %s)",
+                    (srcport,
+                    dstport,
+                    srcIp,
+                    dstIp,
+                    frame[8],  # mac src
+                    frame[9],  # mac dst
+                    applicationLayer,  # application layer
+                    frame[11],  # transport layer
+                    networkLayer,  # network layer
+                    frame[12],  # DNS query
+                    frame[13],  # Info
+                    )
+                )
+
     except KeyboardInterrupt:
         sys.stdout.flush()
         pass

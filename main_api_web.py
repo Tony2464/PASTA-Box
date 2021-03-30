482c7d15b2560cc3728a1ebc059f458a2911a359
XenorInspire <nicolas.fouchard94370@gmail.com>
2021-03-30 21:46:37 +0100

    ADD 404 web page V1

--- main_api_web.py
+++ main_api_web.py
@@ -78,8 +78,11 @@ def test_disconnect():
     print('Client disconnected')
     thread_stop_event.set()
 
-### Web Scocket for Live frames END
+### 404 web page
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('pages/404.html'), 404
 
 if __name__ == "__main__":
     # app.run(host=config.hostConfig, debug=config.debugMode)

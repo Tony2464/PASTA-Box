a55793c1cf21b08f095388db6b17f1614027cd2b
XenorInspire <nicolas.fouchard94370@gmail.com>
2021-03-30 07:43:54 +0100

    HOTFIX rule building for ebtables

--- firewall/customRules.py
+++ firewall/customRules.py
@@ -1,15 +1,16 @@
 import re
 import os
-
+import requests
+import subprocess
 
 # Regex IPv4
-ipv4 = '''^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(  
+ipv4 = """^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(  
             25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(  
             25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(  
-            25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)$'''
+            25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)$"""
 
 # Regex IPv6
-ipv6 = '''(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}| 
+ipv6 = """(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}| 
         ([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:) 
         {1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1 
         ,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4} 
@@ -23,7 +24,12 @@ ipv6 = '''(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|
         ])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0 
         -9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4] 
         |1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4] 
-        |1{0,1}[0-9]){0,1}[0-9]))'''
+        |1{0,1}[0-9]){0,1}[0-9]))"""
+
+# Accorting to the ebtables documentation
+# The --ip-protocol option must be specified as TCP, UDP, DCCP or SCTP
+# Only if a destination/source port is specified
+ebtablesPortProtocols = [6, 17, 33, 132]
 
 
 # Check IP version
@@ -42,36 +48,52 @@ def checkIP(string):
 def verifyRule(Rule):
     ipSrcVersion = 0
     ipDstVersion = 0
-    if(int(Rule["protocol"]) < 0 or int(Rule["protocol"]) > 3):
+    if int(Rule["protocol"]) < 0 or int(Rule["protocol"]) > 3:
         return -1
 
-    if(int(Rule["protocol"]) == 3):
-        if(Rule["ipDst"] == "" and Rule["ipSrc"] == ""):
+    if int(Rule["protocol"]) == 3:
+        if Rule["ipDst"] == "" and Rule["ipSrc"] == "":
             return -2
 
-    if(Rule["portSrc"] == "" and Rule["portDst"] == ""):
-        if(Rule["ipDst"] == "" and Rule["ipSrc"] == ""):
+    if Rule["portSrc"] == "" and Rule["portDst"] == "":
+        if Rule["ipDst"] == "" and Rule["ipSrc"] == "":
             return -3
 
-    if(Rule["ipDst"] != ""):
+    if Rule["ipDst"] != "":
         ipDstVersion = checkIP(Rule["ipDst"])
-        if(ipDstVersion == 3):
+        if ipDstVersion == 3:
             return -4
 
-    if(Rule["ipSrc"] != ""):
+    if Rule["ipSrc"] != "":
         ipSrcVersion = checkIP(Rule["ipSrc"])
-        if(ipSrcVersion == 3):
+        if ipSrcVersion == 3:
             return -5
-        if(Rule["ipVersion"] != ipDstVersion and ipDstVersion != 0):
+        if Rule["ipVersion"] != ipDstVersion and ipDstVersion != 0:
             return -6
 
-    if(ipSrcVersion != 0 and ipDstVersion != 0 and ipDstVersion != ipSrcVersion):
+    if ipSrcVersion != 0 and ipDstVersion != 0 and ipDstVersion != ipSrcVersion:
         return -6
 
-    if(Rule["portSrc"] != "" and (int(Rule["portSrc"]) < 1 or int(Rule["portSrc"]) > 65535)):
+    if (
+        Rule["portSrc"] != ""
+        and Rule["portSrc"] != None
+        and (
+            Rule["portSrc"] == ""
+            or int(Rule["portSrc"]) < 1
+            or int(Rule["portSrc"]) > 65535
+        )
+    ):
         return -7
 
-    if(Rule["portDst"] != "" and (int(Rule["portDst"]) < 1 or int(Rule["portDst"]) > 65535)):
+    if (
+        Rule["portDst"] != ""
+        and Rule["portDst"] != None
+        and (
+            Rule["portDst"] == ""
+            or int(Rule["portDst"]) < 1
+            or int(Rule["portDst"]) > 65535
+        )
+    ):
         return -8
 
     return 0
@@ -81,65 +103,108 @@ def verifyRule(Rule):
 
 def buildCustomRules(Rule):
     error = verifyRule(Rule)
-    if(error < 0):
+    if error < 0:
         return error
 
-    exception = False
-    cmd = "sudo ebtables -t filter -A FORWARD "
-    if(Rule["ipVersion"] == 1):
+    exceptionIP = False
+    exceptionProto = False
+
+    cmd = "/usr/bin/sudo /usr/sbin/ebtables -t filter -A FORWARD "
+    if int(Rule["ipVersion"]) == 1:
         cmd += "-p ipv4 "
-        version = "ipv4"
-        if(Rule["ipSrc"] != None):
+
+        if (Rule["portSrc"] != None and Rule["portSrc"] != "") or (
+            Rule["portDst"] != None and Rule["portDst"] != ""
+        ):
+            cmd += "--ip-protocol "
+            if int(Rule["protocol"]) == 1:
+                cmd += "tcp "
+            elif int(Rule["protocol"]) == 2:
+                cmd += "udp "
+            elif int(Rule["protocol"]) == 2:
+                cmd += "icmp "
+            else:
+                cmd += "all "
+                exceptionProto = True
+
+        if Rule["ipSrc"] != None and Rule["ipSrc"] != "":
             cmd += "--ip-src " + Rule["ipSrc"] + " "
-        if(Rule["ipDst"] != None):
+        if Rule["ipDst"] != None and Rule["ipDst"] != "":
             cmd += "--ip-dst " + Rule["ipDst"] + " "
 
-    elif(Rule["ipVersion"] == 2):
+    elif int(Rule["ipVersion"]) == 2:
         cmd += "-p ipv6 "
-        version = "ipv6"
-        if(Rule["ipSrc"] != None):
+
+        if (Rule["portSrc"] != None and Rule["portSrc"] != "") or (
+            Rule["portDst"] != None and Rule["portDst"] != ""
+        ):
+            cmd += "--ip6-protocol "
+            if int(Rule["protocol"]) == 1:
+                cmd += "tcp "
+            elif int(Rule["protocol"]) == 2:
+                cmd += "udp "
+            elif int(Rule["protocol"]) == 2:
+                cmd += "icmp "
+            else:
+                cmd += "all "
+                exceptionProto = True
+
+        if Rule["ipSrc"] != None and Rule["ipSrc"] != "":
             cmd += "--ip6-source " + Rule["ipSrc"] + " "
-        if(Rule["ipDst"] != None):
+        if Rule["ipDst"] != None and Rule["ipDst"] != "":
             cmd += "--ip6-destination " + Rule["ipDst"] + " "
 
     else:
-        exception = True
+        cmd += "-p ipv4 --ip-protocol "
+        exceptionIP = True
 
-    if(Rule["protocol"] != None):
+    if int(Rule["ipVersion"]) == 1:
 
-        if(Rule["ipVersion"] == 1):
-            cmd += "--ip-protocol "
-            if(Rule["protocol"] == 1):
-                cmd += "tcp "
-            elif(Rule["protocol"] == 2):
-                cmd += "udp "
-            else:
-                cmd += "icmp "
+        if int(Rule["protocol"]) != 3:
+            if Rule["portSrc"] != None and Rule["portSrc"] != "":
+                cmd += "--ip-source-port " + str(Rule["portSrc"]) + " "
+            if Rule["portDst"] != None and Rule["portDst"] != "":
+                cmd += "--ip-destination-port " + str(Rule["portDst"]) + " "
 
-            if(Rule["protocol"] != 3):
-                if(Rule["portSrc"] != None):
-                    cmd += "--ip-source-port " + Rule["portSrc"]
-                else:
-                    cmd += "--ip-destination-port " + Rule["portDst"]
+    elif int(Rule["ipVersion"]) == 2:
 
-        else:
-            cmd += "--ip6-protocol "
-            if(Rule["protocol"] == 1):
-                cmd += "tcp "
-            elif(Rule["protocol"] == 2):
-                cmd += "udp "
-            else:
-                cmd += "icmp "
+        if int(Rule["protocol"]) != 3:
+            if Rule["portSrc"] != None and Rule["portSrc"] != "":
+                cmd += "--ip6-source-port " + str(Rule["portSrc"]) + " "
+            if Rule["portDst"] != None and Rule["portDst"] != "":
+                cmd += "--ip6-destination-port " + str(Rule["portDst"]) + " "
 
-            if(Rule["protocol"] != 3):
-                if(Rule["portSrc"] != None):
-                    cmd += "--ip6-source-port " + Rule["portSrc"]
-                else:
-                    cmd += "--ip6-destination-port " + Rule["portDst"]
     cmd += "-j DROP"
-    res = os.system(cmd)
 
-    if(exception == True):
-        res = os.system(cmd.replace('--ip6-protocol', '--ip-protocol'))
+    if exceptionIP == True and exceptionProto == False:
+        res = os.system(cmd)
+        res = os.system(
+            cmd.replace("-p ipv4 --ip-protocol ", "-p ipv6 --ip6-protocol ")
+        )
+
+    elif exceptionProto == True:
+        cmds = cmd.split("all")
+        for i in range(len(ebtablesPortProtocols)):
+            res = os.system(cmds[0] + str(ebtablesPortProtocols[i]) + cmds[1])
+
+        if exceptionIP == True:
+            cmdIpv6 = cmd.replace("-p ipv4 --ip-protocol ", "-p ipv6 --ip6-protocol ")
+            cmdsIpv6 = cmdIpv6.split("all")
+            for i in range(len(ebtablesPortProtocols)):
+                res = os.system(
+                    cmdsIpv6[0] + str(ebtablesPortProtocols[i]) + cmdsIpv6[1]
+                )
+
+    else:
+        res = os.system(cmd)
 
     return 0
+
+
+# Get all the rules in database
+
+def getAllCustomRules():
+    r = requests.get("http://localhost/api/rules")
+    data = r.json()
+    for i in range(len(data)):
+        buildCustomRules(data[i])

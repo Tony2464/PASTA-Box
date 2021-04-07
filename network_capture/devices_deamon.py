465b48a26d19002c59f3df02c053ca1c3bb21789
Tony2464 <anthony.fargette@yahoo.fr>
2021-04-07 02:03:53 +0200

    UPDATE Insert devices

--- network_capture/devices_deamon.py
+++ network_capture/devices_deamon.py
@@ -3,3 +3,5 @@ from crontabs import Cron, Tab
 # Local imports
 import insert_devices
 
+# print(insert_devices.getUniqueMacAddr())
+print(insert_devices.getLastFrame("00:e0:4c:72:6e:ed")) 
--- network_capture/insert_devices.py
+++ network_capture/insert_devices.py
@@ -1,1 +1,20 @@
 import requests
+import json
+
+# Get all unique and last MAC address
+
+
+def getUniqueMacAddr():
+    r = requests.get('http://localhost/api/frames/macAddr')
+    data = r.json()
+    return data
+
+# Get last frame from MAC address
+
+
+def getLastFrame(macAddr):
+    req = "http://localhost/api/frames"
+    params = {"limit": 1, "macSourceAndDest": macAddr}
+    r = requests.get(req, params)
+    data = r.json()
+    return data

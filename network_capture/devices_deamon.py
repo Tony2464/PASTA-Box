0c3431dc7c30fd8578c2d349c65fc3d9c09dd584
Tony2464 <anthony.fargette@yahoo.fr>
2021-04-09 01:27:31 +0200

    ADD insert new MAC

--- network_capture/devices_deamon.py
+++ network_capture/devices_deamon.py
@@ -4,4 +4,5 @@ from crontabs import Cron, Tab
 import insert_devices
 
 # print(insert_devices.getUniqueMacAddr())
-print(insert_devices.getLastFrame("00:e0:4c:72:6e:ed")) 
+# print(insert_devices.getLastFrame("00:e0:4c:72:6e:ed")) 
+insert_devices.insertNewIp()
--- network_capture/insert_devices.py
+++ network_capture/insert_devices.py
@@ -1,28 +1,73 @@
+from datetime import datetime
+from flask.helpers import get_load_dotenv
 import requests
-import json
+
+# Api url base
+
+
+def apiUrlBase(path):
+    return "http://localhost/api/" + path
+
+# convert string to date format
+
+
+def stringToDate(string):
+    return str(datetime.strptime(string, "%a, %d %b %Y %H:%M:%S %Z"))
+
+# Insert data with API
+
+
+def insertData(url, params=None):
+    requests.post(url, json=params)
+    return 0
+
+
+def getData(url, params=None):
+    r = requests.get(url, params)
+    data = r.json()
+    return data
+
 
 # Get all unique and last MAC address
 
-
-def getUniqueMacAddr():
-    r = requests.get('http://localhost/api/frames/macAddr')
-    data = r.json()
-    return data
+# def getUniqueMacAddr():
+#     r = requests.get('http://localhost/api/frames/macAddr')
+#     data = r.json()
+#     return data
 
 # Get MAC to insert
 
 
-def macToInsert():
-    r = requests.get('http://localhost/api/frames/macToInsert')
-    data = r.json()
-    return data
+def getMacToInsert():
+    return getData(apiUrlBase("frames/macToInsert"))
 
 # Get last frame from MAC address
 
 
 def getLastFrame(macAddr):
-    req = "http://localhost/api/frames"
     params = {"limit": 1, "macSourceAndDest": macAddr}
-    r = requests.get(req, params)
-    data = r.json()
-    return data
+    return getData(apiUrlBase("frames"), params)
+
+# Get all devices
+
+
+def getDevices():
+    return getData(apiUrlBase("devices"))
+
+
+# Insert new mac
+
+
+def insertNewMac():
+    macs = getMacToInsert()
+    for i in range(0, len(macs)):
+        params = {
+            "macAddr": macs[i]["macAddr"],
+            "firstConnection": stringToDate(macs[i]["date"])
+        }
+        insertData(apiUrlBase("devices"), params)
+    return 0
+
+def insertNewIp():
+    devices = getDevices()
+    return 0

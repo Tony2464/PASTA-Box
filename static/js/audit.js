bfb953c9fac5dc38d00c586aef9cf4d0e3a6bd35
XenorInspire <nicolas.fouchard94370@gmail.com>
2021-06-15 09:26:48 +0100

    ADD buttons depending of the audit mode

--- static/js/audit.js
+++ static/js/audit.js
@@ -1,3 +1,11 @@
+var auditMode = 0;
+
+function updateMode(value) {
+
+    window.auditMode = value;
+
+}
+
 function changeAuditMode(mode) {
 
     switch (mode) {
@@ -21,6 +29,24 @@ function loadAutoMode() {
     description.style.display = "block";
     disableDescriptions(0);
 
+    let changeButton = document.getElementById('changeModeButton');
+    let buttonMode2 = document.getElementById('buttonMode2');
+    let buttonMode3 = document.getElementById('buttonMode3');
+
+    if (auditMode != 1) {
+        console.log(auditMode);
+        changeButton.style.display = "block";
+        buttonMode2.style.display = "none";
+        buttonMode3.style.display = "none";
+
+    } else {
+
+        changeButton.style.display = "none";
+        buttonMode2.style.display = "none";
+        buttonMode3.style.display = "none";
+
+    }
+
 }
 
 function loadSemiAutoMode() {
@@ -29,6 +55,24 @@ function loadSemiAutoMode() {
     description.style.display = "block";
     disableDescriptions(1);
 
+    let changeButton = document.getElementById('changeModeButton');
+    let buttonMode2 = document.getElementById('buttonMode2');
+    let buttonMode3 = document.getElementById('buttonMode3');
+
+    if (auditMode != 2) {
+
+        changeButton.style.display = "block";
+        buttonMode2.style.display = "none";
+        buttonMode3.style.display = "none";
+
+    } else {
+
+        changeButton.style.display = "none";
+        buttonMode2.style.display = "block";
+        buttonMode3.style.display = "none";
+
+    }
+
 }
 
 function loadManualMode() {
@@ -36,6 +80,25 @@ function loadManualMode() {
     let description = document.getElementById('des3');
     description.style.display = "block";
     disableDescriptions(2);
+
+    let changeButton = document.getElementById('changeModeButton');
+    let buttonMode2 = document.getElementById('buttonMode2');
+    let buttonMode3 = document.getElementById('buttonMode3');
+
+    if (auditMode != 3) {
+
+        changeButton.style.display = "block";
+        buttonMode2.style.display = "none";
+        buttonMode3.style.display = "none";
+
+    } else {
+
+        changeButton.style.display = "none";
+        buttonMode2.style.display = "none";
+        buttonMode3.style.display = "block";
+
+    }
+
 
 }
 
